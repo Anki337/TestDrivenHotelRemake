ef10bb60916b451027555e8178eb2c3445b32a16
David Ankner <david.ankner@LIVE.SE>
2024-01-26 13:01:37 +0100

    Add project files.

---
+++ TestDrivenHotelRemake.BLL/ReservationService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDrivenHotelRemake.BLL
+{
+    public class ReservationService
+    {
+        private List<string> reservedRooms = new List<string>();
+        private List<string> hotelRooms = new List<string> { "Room1", "Room2", "Room3" };
+
+        public void AddReservation(string room)
+        {
+            if (hotelRooms.Contains(room))
+            {
+                hotelRooms.Remove(room);
+                reservedRooms.Add(room);
+            }
+        }
+
+        public void CancelReservation(string room)
+        {
+            if (reservedRooms.Contains(room))
+            {
+                reservedRooms.Remove(room);
+                hotelRooms.Add(room);
+            }
+        }
+
+        public List<string> GetReservedRooms()
+        {
+            return reservedRooms;
+        }
+
+        public List<string> GetHotelRooms()
+        {
+            return hotelRooms;
+        }
+    }
+}
---
+++ TestDrivenHotelRemake.Tests/ReservationServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDrivenHotelRemake.BLL;
+
+namespace TestDrivenHotelRemake.Tests
+{
+    public class ReservationServiceTests
+    {
+        private ReservationService reservationService;
+
+        public ReservationServiceTests()
+        {
+            reservationService = new ReservationService();
+        }
+
+        [Fact]
+        public void AddReservation_WhenRoomIsAvailable_ShouldAddReservation()
+        {
+            // Given
+            string room = "Room1";
+            // When
+            reservationService.AddReservation(room);
+            // Then
+            reservationService.GetReservedRooms().Should().Contain(room);
+        }
+        
+        [Fact]
+        public void AddReservation_WhenRoomIsAvailable_ShouldRemoveRoomFromHotelRooms()
+        {
+            // Given
+            string room = "Room1";
+
+            // When
+            reservationService.AddReservation(room);
+
+            // Then
+            reservationService.GetHotelRooms().Should().NotContain(room);
+        }
+        [Fact]
+        public void AddReservation_WhenRoomIsNotAvailable_ShouldNotAddReservation()
+        {
+            
+        }
+        //[Fact]
+        //public void AddReservation_WhenRoomIsNotAvailable_ShouldNotRemoveRoomFromHotelRooms()
+        //{
+            
+        //}
+        //[Fact]
+        //public void CancelReservation_WhenRoomIsReserved_ShouldCancelReservation()
+        //{
+           
+        //}
+
+
+
+    }
+}
